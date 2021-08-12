462c3286ebda563865c460374beb4c203ccc4191
Anton Daneyko <1176545+mezhaka@users.noreply.github.com>
2021-08-12 15:43:23 +0200

    Add mp3 to flac conversion

--- transcribe.sh
+++ transcribe.sh
@@ -3,18 +3,26 @@ set -u
 set -e
 set -x
 
+
 previous_account=`gcloud auth list | grep "^\* " | awk '{print $2;}'` 2>/dev/null
 gcloud config set account speech-to-text@speech-api-173412.iam.gserviceaccount.com
 
-#input_path=$1
 for input_path in $@
 do
-    echo $input_path
-    gsutil cp $input_path gs://com_mezhaka_input_audio
+    if [ "${input_path: -4}" == ".mp3" ]
+    then
+        to_upload=${input_path}.flac
+        ffmpeg -i ${input_path} -ac 1 -y $to_upload
+    else
+        to_upload=${input_path}
+    fi
 
-    structured_output="structured.${input_path}.txt"
-    gcloud ml speech recognize-long-running "gs://com_mezhaka_input_audio/${input_path}" --language-code=ru-RU > $structured_output
-    cat $structured_output | jq '.results[].alternatives[].transcript' > "removed-structure${input_path}.txt"
+    echo $to_upload
+    gsutil cp $to_upload gs://com_mezhaka_input_audio
+
+    structured_output="structured.${to_upload}.txt"
+    gcloud ml speech recognize-long-running "gs://com_mezhaka_input_audio/${to_upload}" --language-code=ru-RU > $structured_output
+    cat $structured_output | jq '.results[].alternatives[].transcript' > "removed-structure.${to_upload}.txt"
 done
 
 gcloud config set account $previous_account
