5c04b3d923a099f2b0de72b34b63525ee24824df
Anton Daneyko <1176545+mezhaka@users.noreply.github.com>
2021-08-19 17:49:19 +0200

    Fix transcribe script to be able to use file names with space in it

--- transcribe.sh
+++ transcribe.sh
@@ -7,22 +7,22 @@ set -x
 previous_account=`gcloud auth list | grep "^\* " | awk '{print $2;}'` 2>/dev/null
 gcloud config set account speech-to-text@speech-api-173412.iam.gserviceaccount.com
 
-for input_path in $@
+for input_path in "$@"
 do
     if [ "${input_path: -4}" == ".mp3" ]
     then
-        to_upload=${input_path}.flac
-        ffmpeg -i ${input_path} -ac 1 -y $to_upload
+        to_upload="${input_path}".flac
+        ffmpeg -i "${input_path}" -ac 1 -y "$to_upload"
     else
-        to_upload=${input_path}
+        to_upload="${input_path}"
     fi
 
-    echo $to_upload
-    gsutil cp $to_upload gs://com_mezhaka_input_audio
+    echo "$to_upload"
+    gsutil -o GSUtil:parallel_composite_upload_threshold=150M cp "$to_upload" gs://com_mezhaka_input_audio
 
     structured_output="structured.${to_upload}.txt"
-    gcloud ml speech recognize-long-running "gs://com_mezhaka_input_audio/${to_upload}" --language-code=ru-RU > $structured_output
-    cat $structured_output | jq '.results[].alternatives[].transcript' > "removed-structure.${to_upload}.txt"
+    gcloud ml speech recognize-long-running "gs://com_mezhaka_input_audio/${to_upload}" --language-code=ru-RU > "$structured_output"
+    cat "$structured_output" | jq '.results[].alternatives[].transcript' > "removed-structure.${to_upload}.txt"
 done
 
 gcloud config set account $previous_account
